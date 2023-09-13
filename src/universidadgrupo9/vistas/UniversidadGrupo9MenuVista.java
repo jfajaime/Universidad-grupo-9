89a993c520534902e94877c4e9500e3a803d3f52
Ched2370 <mmibz.85@gmail.com>
2023-09-13 18:43:55 -0300

    eliminando la e de menu

--- src/universidadgrupo9/vistas/UniversidadGrupo9MenuVista.java
+++ src/universidadgrupo9/vistas/UniversidadGrupo9MenuVista.java
@@ -9,12 +9,12 @@ package universidadgrupo9.vistas;
  *
  * @author jfaja
  */
-public class UniversidadGrupo9MeenuVista extends javax.swing.JFrame {
+public class UniversidadGrupo9MenuVista extends javax.swing.JFrame {
 
     /**
      * Creates new form UniversidadGrupo9Vista
      */
-    public UniversidadGrupo9MeenuVista() {
+    public UniversidadGrupo9MenuVista() {
         initComponents();
     }
 
@@ -241,14 +241,22 @@ public class UniversidadGrupo9MeenuVista extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(UniversidadGrupo9MeenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UniversidadGrupo9MenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(UniversidadGrupo9MeenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UniversidadGrupo9MenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(UniversidadGrupo9MeenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UniversidadGrupo9MenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(UniversidadGrupo9MeenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UniversidadGrupo9MenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -261,7 +269,7 @@ public class UniversidadGrupo9MeenuVista extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new UniversidadGrupo9MeenuVista().setVisible(true);
+                new UniversidadGrupo9MenuVista().setVisible(true);
             }
         });
     }
