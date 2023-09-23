fc40b5164ee52b61396349e91c9ccb3cb243d5a7
joselesc <joselescanogm@gmail.com>
2023-09-23 20:53:51 -0300

    Agregado buscar por nombre en listar materia

--- src/universidadgrupo9/vistas/ListadoDeMaterias.java
+++ src/universidadgrupo9/vistas/ListadoDeMaterias.java
@@ -22,15 +22,13 @@ public class ListadoDeMaterias extends javax.swing.JInternalFrame {
             return false;
         }
     };
-    
+
     public ListadoDeMaterias() {
         initComponents();
         armarCabecera();
         listarMaterias();
     }
 
-  
-      
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -38,6 +36,8 @@ public class ListadoDeMaterias extends javax.swing.JInternalFrame {
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTListaMaterias = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
+        jTBuscarPorNombre = new javax.swing.JTextField();
 
         setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(255, 0, 0)));
         setClosable(true);
@@ -63,6 +63,15 @@ public class ListadoDeMaterias extends javax.swing.JInternalFrame {
         jTListaMaterias.setRowHeight(20);
         jScrollPane1.setViewportView(jTListaMaterias);
 
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 15)); // NOI18N
+        jLabel2.setText("Busqueda por nombre");
+
+        jTBuscarPorNombre.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jTBuscarPorNombreKeyReleased(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -70,51 +79,94 @@ public class ListadoDeMaterias extends javax.swing.JInternalFrame {
             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 798, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 609, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(88, 88, 88))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(44, 44, 44)
+                        .addComponent(jTBuscarPorNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 284, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(138, 138, 138))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 609, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(85, 85, 85))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(33, 33, 33)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 65, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTBuscarPorNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(43, 43, 43)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(43, 43, 43))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jTBuscarPorNombreKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTBuscarPorNombreKeyReleased
+
+        borrarFilas();
+         MateriaData md = new MateriaData();
+      
+        for (Materia mat :  md.listarMaterias()) {
+
+            if (mat.getNombre().toLowerCase().startsWith(jTBuscarPorNombre.getText().toLowerCase())) {
+
+                modelo.addRow(new Object[]{
+                   mat.getId(),
+                   mat.getNombre(),
+                   mat.getAnio(),
+                   mat.isEstado()
+
+                });
+            }
+        }
+
+
+    }//GEN-LAST:event_jTBuscarPorNombreKeyReleased
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTBuscarPorNombre;
     private javax.swing.JTable jTListaMaterias;
     // End of variables declaration//GEN-END:variables
 
-    
- private void listarMaterias(){
-     
-   MateriaData md=new MateriaData();
-   
-   for(Materia m:md.listarMaterias())
-       
-   modelo.addRow(new Object[]{
-                   m.getId(),
-                   m.getNombre(),
-                   m.getAnio(),
-                   m.isEstado()
-   
-           }); 
- 
-}
-    
-  private void armarCabecera() {
+    private void listarMaterias() {
+
+        MateriaData md = new MateriaData();
+
+        for (Materia m : md.listarMaterias()) {
+            modelo.addRow(new Object[]{
+                m.getId(),
+                m.getNombre(),
+                m.getAnio(),
+                m.isEstado()
+
+            });
+        }
+
+    }
+
+    private void armarCabecera() {
 
         modelo.addColumn("Id Materia");
         modelo.addColumn("Nombre");
         modelo.addColumn("Año");
         modelo.addColumn("Estado");
         jTListaMaterias.setModel(modelo);
+    }
+
+    private void borrarFilas() {
+
+        int filas = jTListaMaterias.getRowCount() - 1;
+        for (int f = filas; f >= 0; f--) {
+
+            modelo.removeRow(f);
+        }
     }
 }
